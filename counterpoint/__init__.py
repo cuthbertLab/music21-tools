8f8d9c796a3bb113267f3435c758da940c8d29ee
Michael Scott Cuthbert <cuthbert@mit.edu>
2017-08-27 13:11:10 -0400

    add demos to music21-demos

---
+++ counterpoint/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ["species"]
+
+from music21.alpha.counterpoint import species
+#------------------------------------------------------------------------------
+# eof
+
---
+++ trecento/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+__all__ = ['cadencebook', 'capua', 'findTrecentoFragments', 'notation', 'tonality']
+
+# this is necessary to get these names available with a
+# from music21 import * import statement
+import sys
+
+from . import cadencebook
+from . import capua
+from . import findTrecentoFragments
+from . import notation
+from . import tonality
+
+#from music21.trecento import *
+
+#------------------------------------------------------------------------------
+# eof
